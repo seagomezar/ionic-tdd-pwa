25fd64268126ff9929be4f02fd572409c96d75bc
Sebastian Alonso Gomez Arias <s.gomez@CO-IT00719.local>
2018-04-19 12:59:22 -0500

    fixing decorator in home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,19 +9,19 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
 import { HoldingsProvider } from '../providers/holdings/holdings';
 
 @NgModule({
   declarations: [
-    MyApp,
-    HomePage
+    MyApp
   ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp, {
       preloadModules: true
-    })
+    }),
+    IonicStorageModule.forRoot(),
+    HttpClientModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
